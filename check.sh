2f021deec93f2328c6370e94af666bb3a22883ab
abhishekmalakar1 <116427959+abhishekmalakar1@users.noreply.github.com>
2022-10-25 09:56:23 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -4,6 +4,9 @@ hostname=$(hostname)
 date=$(date "+%Y-+%m-+%d %H:%M:%S")
 cpu=$(top -b -n 1 -d1|grep "Cpu(s)"|awk '{print $2}'|awk -F. 'print $1')
 mem=$(free|grep Mem|awk '{print $3/$2 * 100.0}')
+uptm=$(uptime)
 
-echo 'hostname          date&time       cpu-usage       mem(%)' >> /tmp/report
-echo "$hostname         $date           $cpu            $mem " >> /tmp/report
+printf "hostname: $hostname\nuptime: $uptm\ndate&time: $date\ncpu-usage: $cpu\nmem(%): $mem" >> /tmp/report
+
+#echo 'hostname          date&time       cpu-usage       mem(%)' >> /tmp/report
+#echo "$hostname         $date           $cpu            $mem " >> /tmp/report
